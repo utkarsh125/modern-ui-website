8e01a5a69070972a333504d1f6158b6c5b5b8bad
Utkarsh Pandey <utk2665@gmail.com>
2023-10-04 15:18:47 +0530

    React Setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { ReactDOM } from "react";//Allows us to hook our react app to the index.html
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('root'));
