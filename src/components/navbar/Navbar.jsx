67a66d0fd9d89663c8c54d8f565e85d426b21358
Utkarsh Pandey <utk2665@gmail.com>
2023-10-06 23:46:47 +0530

    Finding Pivot in an Array

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri'
-import './navbar.css'
-import
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.svg'
+import './navbar.css';
 // BEM ---> Block Element Modifier
 
 const Navbar = () => {
   return (
-    <div className="gpt3__navbar">
+    <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
         <div className='gpt3__navbar-links_logo'>
           <img src={logo} alt="logo" />
