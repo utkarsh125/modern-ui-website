e430a062b68cb633b7f36dc85a3419b3f9ab9f8f
Utkarsh Pandey <utk2665@gmail.com>
2023-10-21 18:41:16 +0530

    Added Brand Logos

---
+++ src/components/brand/imports.js
@@ -0,0 +1,14 @@
+import google from '../../assets/google.png';
+import dropbox from '../../assets/dropbox.png';
+import atlassian from '../../assets/atlassian.png';
+import shopify from '../../assets/shopify.png';
+import slack from '../../assets/slack.png';
+
+
+export {
+    google,
+    slack,
+    atlassian,
+    shopify,
+    dropbox
+}
